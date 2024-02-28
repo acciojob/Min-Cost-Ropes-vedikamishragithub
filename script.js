a17be3756f8366d9942310946c833e9c87cf73d9
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-02-28 23:35:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function mincost(arr)
 { 
 //write your code here
+	arr.sort(function(a, b){return a - b});
+	arr.forEach((item,i)=>{
+		sum+=arr;
+		if(i<0){
+         cost+=sum;
+		}
+	})
+	
+	
+	
+
 // return the min cost
+	return mincost;
   
 }
 
