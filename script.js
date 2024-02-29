766bdf8d467ae6fc98dacb57cdcd27950edfdafe
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-02-29 18:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 
 
-
 function mincost(arr) { 
-	 let cost = 0;       
-    
-        arr.sort(function(a, b){return a - b});
-    
-		while(arr.length > 1) {
+    let cost = 0;
+
+    arr.sort(function(a, b){return a - b});
+
+    while(arr.length > 1) {
         let sum = arr[0] + arr[1];
-        cost += sum;   
+        cost += sum;
         arr = arr.slice(2);
-        arr.push(sum);       
+        arr.push(sum);
         arr.sort(function(a, b){return a - b});
-          return cost;
-		
-        }
- return cost;
+    }
+  
+    return cost;
 }
+
 mincost([4,3,2,6]);
 module.exports=mincost;
